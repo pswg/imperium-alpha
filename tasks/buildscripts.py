69a02b166ccf05537682312aa65fece9703fe0f5
Unknown <patrick.kohler@gmail.com>
2017-06-10 08:03:42 -0500

    Better Formatting in Codex Sagatus
    Created a VS Code task to generate the Codex Sagatus from the Rhapsodia Vacui with correct fromatting.

---
+++ tasks/buildscripts.py
@@ -0,0 +1,31 @@
+import os
+import os.path
+
+SQL_COPYRIGHT_NOTICE = """
+-- Copyright (c) Patrick Kohler
+-- Licensed under the MIT License.
+-- See License.txt in the project root for license information.
+""".strip()
+
+# 1.2 - codex_sagatus.sql
+def generate_codex_sagatus():
+  print("Generating '1.2 - codex_sagatus.sql' ... ", end='', flush=True)
+  with open('Part 1 - Creation\\1.1 - rhapsodia_vacui.sql') as f:
+    content = f.readlines()
+
+  with open('Part 1 - Creation\\1.2 - codex_sagatus.sql', 'w') as f:
+    print('', file=f)
+    print(SQL_COPYRIGHT_NOTICE, file=f)
+    print('', file=f)
+
+    print('ALTER PROCEDURE [Sacrum].[CodexSagatus]', file=f)
+    print('AS', file=f)
+    for line in content:
+      line = line.replace("'", "''").rstrip()
+      if not line.startswith('--'):
+        print("  PRINT '" + line + "';", file=f)
+    print('BEGIN', file=f)
+
+  print("Done.")
+
+generate_codex_sagatus()
